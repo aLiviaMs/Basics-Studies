d76175bf4ea8733f25f899941982ace82123e08e
Lívia Minucelli <liviawrk@gmail.com>
2021-02-05 19:54:39 -0300

    CadastroUser
    Um cadastro de usuario, com verificação de senha e email e um layout estilizado com animações feitas no CSS3

---
+++ userRegister/javascript/register.js
@@ -0,0 +1,20 @@
+var emailsEx = ['livinha@gmail.com','teste@hotmail.com','uuu@gmail.com','abcd@gmail.com','dcba@hotmail.com','abcd@hotmail.com']
+var emailUser = document.getElementById('email').value;
+var password = document.getElementById('password').value;
+
+/*function validateEmail(){
+    for(var i = 0; i<emailsEx.length; i++){
+        if(emailsEx[i].length == emailUser){
+            alert("ERRO");
+        } else{
+            alert("OK");
+        }
+    }
+}
+function validatePassword(){
+    if(password.length < 6 || password.length > 20){
+        alert("Senha inválida");
+    } else{
+        alert("Senha válida");
+    }
+} */

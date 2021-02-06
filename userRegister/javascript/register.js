0ae455289da84040e7c540b39a8498350f6da0de
Lívia Minucelli <liviawrk@gmail.com>
2021-02-06 15:56:05 -0300

    Changes On Script

--- userRegister/javascript/register.js
+++ userRegister/javascript/register.js
@@ -1,20 +1,22 @@
-var emailsEx = ['livinha@gmail.com','teste@hotmail.com','uuu@gmail.com','abcd@gmail.com','dcba@hotmail.com','abcd@hotmail.com']
-var emailUser = document.getElementById('email').value;
-var password = document.getElementById('password').value;
+var emailsEx = ['liv@gmail.com','teste@hotmail.com','uuu@gmail.com','abcd@gmail.com','dcba@hotmail.com','abcd@hotmail.com']
 
-/*function validateEmail(){
+function validadeRegister(){
+    var password = document.getElementById('password').value;
+    var aNumber = /[0-9]/;
+    var aLetter = /[A-Za-z]/;
+    var emailUser = document.getElementById('emailId').value;
+    
     for(var i = 0; i<emailsEx.length; i++){
-        if(emailsEx[i].length == emailUser){
+        if(emailsEx[i] == emailUser){
             alert("ERRO");
         } else{
             alert("OK");
         }
     }
-}
-function validatePassword(){
-    if(password.length < 6 || password.length > 20){
+    
+    if(password.length < 6 || password.length > 20 || password.search(aNumber) == -1 || password.search (aLetter) == -1){
         alert("Senha inválida");
     } else{
         alert("Senha válida");
     }
-} */
+}
